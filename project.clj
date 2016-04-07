ce299a1085e339156c2a19ebac7351d969dd15aa
Sergio Alvarez-Napagao <tranchis@gmail.com>
2016-04-07 17:05:41 +0100

    Remove unused dependency

--- project.clj
+++ project.clj
@@ -6,5 +6,4 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [tranchis/photon-db "0.9.31"]
                  [cheshire "5.5.0"]
-                 [clj-http "1.1.2"]
                  [clj-time "0.11.0"]])
