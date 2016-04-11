bdab3c1cff1642e87ea5949b0bf5b8c2a9de3b32
Sergio Alvarez-Napagao <tranchis@gmail.com>
2016-04-11 17:53:29 +0100

    Update to event schema v1

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject tranchis/photon-file "0.9.42"
+(defproject tranchis/photon-file "0.9.45"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [tranchis/photon-db "0.9.31"]
+                 [tranchis/photon-db "0.9.45"]
                  [cheshire "5.5.0"]
                  [clj-time "0.11.0"]])
