2b541baa765c6a811b581300f69aef30bb3802c2
Sergio Alvarez-Napagao <tranchis@gmail.com>
2017-06-01 01:16:57 +0200

    Upgrade dependencies

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject tranchis/photon-file "0.10.4"
+(defproject tranchis/photon-file "0.10.8"
   :description "Plugin for support for files as DB backend in photon"
   :url "https://github.com/microserviceux/photon-file"
   :license {:name "GNU Affero General Public License Version 3"
             :url "https://www.gnu.org/licenses/agpl-3.0.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [tranchis/photon-db "0.10.4"]
-                 [cheshire "5.7.0"]
+                 [tranchis/photon-db "0.10.8"]
+                 [cheshire "5.7.1"]
                  [clj-time "0.13.0"]
                  [midje "1.8.3"]])
